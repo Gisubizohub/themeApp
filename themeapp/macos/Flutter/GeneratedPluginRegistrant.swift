0094fa0d5eb47a1d814b8fbf8f35fa7aa65e4c25
gisubizo <nicegisubizo@gmail.com>
2024-02-26 17:20:37 +0200

    working with Apis

--- themeapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ themeapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,20 @@ import Foundation
 
 import connectivity_macos
 import connectivity_plus
+import file_selector_macos
+import google_sign_in_ios
 import network_info_plus
 import shared_preferences_foundation
 import smart_auth
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
